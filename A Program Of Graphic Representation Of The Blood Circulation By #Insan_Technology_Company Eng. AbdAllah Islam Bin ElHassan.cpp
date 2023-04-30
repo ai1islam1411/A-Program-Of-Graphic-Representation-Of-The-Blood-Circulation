cf68e2b94fe7380e7d5889da8580e2326c298acb
Islam Bin ElHassan <108907313+ai1islam1411@users.noreply.github.com>
2023-04-30 05:39:51 +0200

    Update A Program Of Graphic Representation Of The Blood Circulation By #Insan_Technology_Company Eng. AbdAllah Islam Bin ElHassan.cpp

--- A Program Of Graphic Representation Of The Blood Circulation By #Insan_Technology_Company Eng. AbdAllah Islam Bin ElHassan.cpp
+++ A Program Of Graphic Representation Of The Blood Circulation By #Insan_Technology_Company Eng. AbdAllah Islam Bin ElHassan.cpp
@@ -1,6 +1,7 @@
 /* A Program Of Graphic Representation Of The Blood Circulation By #Insan_Technology_Company 
 Eng. AbdAllah Islam Bin ElHassan
-https://github.com/ai1islam1411/A-Program-Of-Graphic-Representation-Of-The-Blood-Circulation */
+https://github.com/ai1islam1411/A-Program-Of-Graphic-Representation-Of-The-Blood-Circulation 
+https://insantechnology.com */
 
 #include<stdio.h>
 #include<stdlib.h>
